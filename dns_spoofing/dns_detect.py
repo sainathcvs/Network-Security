64d811079b1f6e1a99454ca257e3e85fdf4c9151
sainathcvs <sainath.cvs@gmail.com>
2017-12-07 22:40:54 -0500

    latest changes for pcap pasring

--- dns_spoofing/dns_detect.py
+++ dns_spoofing/dns_detect.py
@@ -3,13 +3,11 @@
 from scapy.all import *
 import dpkt
 from sys import argv
-
+import pcap
 
 def have_common_ips(existing_ips,current_ips):
-    print "-------------have_common_ips-------------------"
+    #print "-------------have_common_ips-------------------"
     len_min = min(len(existing_ips),len(current_ips))
-    print "existing---",existing_ips
-    print "current---", current_ips
     if len_min == len(existing_ips):
         for i in xrange(0,len_min):
             if existing_ips[i] in current_ips:
@@ -21,15 +19,17 @@ def have_common_ips(existing_ips,current_ips):
     return False
 
 
-def print_dns_attack(txID,req,ans1,ans2):
-    print "-------------print_dns_attack-------------------"
+def print_dns_attack(txID,req,ans1,ans2,timestamp):
+    #print "-------------print_dns_attack-------------------"
+    print timestamp,'DNS poisoning attempt'
     print 'TXID ',txID,' Request ',req
     print 'Answer1 ',ans1
     print 'Answer2 ', ans2
+    print '\n'
 
 
 def dns_detect(pkt):
-    print "-------------dns_detect-------------------"
+    #print "-------------dns_detect-------------------"
     dns = pkt[DNS]
     txID = dns.id
     ans_ips=[]
@@ -38,21 +38,19 @@ def dns_detect(pkt):
         if(dns.qr ==  dpkt.dns.DNS_Q and dns.qdcount == 1 and dns.nscount == 0 and dns.ancount == 0):
             #query packet
             if txID not in txnIDs:
-                print "not txid query----",txID,dns.qd[0].qname[:-1]
-                txnIDs[txID] = {'req':dns.qd[0].qname[:-1],'ips':[]}
+                txnIDs[txID] = {'req':dns.qd[0].qname[:-1],'ips':[],'timestamp':" "}
         else:
             #answer packet
             if txID not in txnIDs.keys():
                 print "if ancount----",txID,dns.ancount,dns.qd[0].qname[:-1]
                 for i in xrange(0,dns.ancount):
-                    print "ancount----",dns.an[i].rdata
                     ans_ips.append(dns.an[i].rdata)
-                txnIDs[txID] = {'req':dns.qd[0].qname[:-1],'ips':ans_ips}
+                txnIDs[txID] = {'req':dns.qd[0].qname[:-1],'ips':ans_ips,'timestamp':" "}
             else:
                 existing_ips = txnIDs[txID]['ips']
-                print "else ancount----",txID,dns.ancount,dns.qd[0].qname[:-1],len(existing_ips)
+                timestamp = pkt.time
+                #print "else ancount----",txID,dns.ancount,dns.qd[0].qname[:-1],len(existing_ips)
                 for i in xrange(0,dns.ancount):
-                    print "else ancount----",dns.an[i].rdata
                     ans_ips.append(dns.an[i].rdata)
                 if len(existing_ips)==0:
                     txnIDs[txID]['ips'] = ans_ips
@@ -60,7 +58,7 @@ def dns_detect(pkt):
                     if len(existing_ips)>0:
                         is_similar = have_common_ips(existing_ips,ans_ips)
                         if not is_similar:
-                            print_dns_attack(txID,dns.qd[0].qname[:-1],existing_ips,ans_ips)
+                            print_dns_attack(txID,dns.qd[0].qname[:-1],existing_ips,ans_ips,timestamp)
 
 if __name__=='__main__':
     args={}
@@ -70,7 +68,6 @@ if __name__=='__main__':
     is_interface_given = False
     global is_tracefile_given
     is_tracefile_given = False
-    print "c---",len(argv)
     while argv:
         if argv[0][0] == '-':
             args[argv[0]]=argv[1]
@@ -78,9 +75,13 @@ if __name__=='__main__':
     if '-i' in args:
         is_interface_given = True
         interface = args['-i']
-        print(args['-i'])
     if '-r' in args:
         is_tracefile_given= True
-        hostfile = args['-r']
-        print(args['-r'])
-    pkts = sniff(iface=interface, filter="udp port 53",prn=dns_detect)
+        tracefile = args['-r']
+    if is_tracefile_given:
+        packets = rdpcap(tracefile)
+        for pkt in packets:
+            if pkt.haslayer(DNS):
+                dns_detect(pkt)
+    else:
+        pkts = sniff(iface=interface, filter="udp port 53",prn=dns_detect)
